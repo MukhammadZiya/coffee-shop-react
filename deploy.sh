8b8dfe13bff7ff1b7ff72f11efa01817f204b67b
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-01-30 22:49:00 +0900

    feat: deploy  project via shell & pm2

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#PRODUCTION 
+# git reset --hard 
+# git checkout master 
+# git pull origin master 
+
+
+npm i yarn -g 
+yarn global add serve 
+yarn 
+yarn run build 
+# cd C:\Users\Muhammadziyo\Desktop\cofeshop-react
+# pm2 start yarn --name "CoffeeShoppy-React" -- run start:prod
+# pm2 start yarn --name "CoffeeShoppy-React" -- run start:prod
+# pm2 start "yarn run start:prod" --name=CoffeeShoppy-React
+# pm2 start serve --name="CoffeeShoppy-React" -- -s build -l 80
+pm2 start process.config.js
+
